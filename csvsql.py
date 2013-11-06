5c64e3b3645e034f8ad7674c55552df2dc6b1016
ahaic <hansoncao@live.com>
2013-11-06 16:44:21 +0800

    works

--- csvsql.py
+++ csvsql.py
@@ -12,32 +12,45 @@ with open('d:\Hanson\id.csv', newline='',encoding='utf-8') as f:
     for i in reader:
         print(i)
         print(type(i[7]))
-        sql=("INSERT INTO usr "
-             "(name,id_type,id,Gender,Birthday,Address,District2,Mobile,Tel,Email,Nation,Education,Company,Version,uid)"
-             "VALUES(%(name)s,$(id_type)s,%(id)s,$(Gender)s,%(Birthday)s,%(Address)s,$(District2)s,%(Mobile)s,$(Tel)s,%(Email)s,$(Nation)s,%(Edication)s,$(Company)s,%(Version)s),%(uid)s")
-        data={
-            'name':i[0],
-            'id_type':i[3],
-            'id':i[4],
-            'Gender':i[5],
-            'Birthday':i[6],
-            'Address':i[7],
-            'District':i[11],
-            'Mobile':i[19],
-            'Tel':i[20],
-            'Email':i[22],
-            'Nation':i[23],
-            'Education':i[25],
-            'Company':i[26],
-            'Version':i[31],
-            'uid':i[32],
-            }
+        sql = ("INSERT INTO usr "
+               "(name, id_type, id, Gender, Birthday,Address,District2,Mobile,Tel,Email,Nation,uid) "
+               "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+               
+
+        
+        data=(i[0],i[3],i[4],i[5],i[6],i[7],i[11],i[19],i[20],i[22],i[23],i[32])
+        print(data)
         try:
             cursor.execute(sql,data)
+            cnx.commit()
         except mysql.connector.Error as e:
             print("insert data --",e.msg)
-            break
+            breaka
         
         
     
     
+    
+
+
+
+
+
+
+
+'''
+import csv
+with open('d:\Hanson\id.csv', newline='',encoding='utf-8') as f:
+    reader = csv.reader(f)
+    for i in reader:
+        print(i)
+        
+'''
+
+
+'''
+for i in reader:
+        print(i)
+        break
+    
+'''
