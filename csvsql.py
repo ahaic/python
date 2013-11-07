af379efdb2d12c1753eeb0d607af4aaa8189e244
ahaic <hansoncao@live.com>
2013-11-07 14:18:56 +0800

    insert sql exception

--- csvsql.py
+++ csvsql.py
@@ -7,50 +7,37 @@ cnx = mysql.connector.connect(user='root', password='admin',
 
 cursor=cnx.cursor()
 
+
+def insert():
+    sql = ("INSERT INTO usr "
+           "(name, id_type, id, Gender, Birthday,Address,District2,Mobile,Tel,Email,Nation,uid) "
+           "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+    
+
+    try:
+        cursor.execute(sql,data)
+        cnx.commit()
+    except mysql.connector.Error as e:
+        print("insert data --",e.msg)
+     
+
+
+
 with open('d:\Hanson\id.csv', newline='',encoding='utf-8') as f:
     reader = csv.reader(f)
+    
     for i in reader:
-        print(i)
-        print(type(i[7]))
-        sql = ("INSERT INTO usr "
-               "(name, id_type, id, Gender, Birthday,Address,District2,Mobile,Tel,Email,Nation,uid) "
-               "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
-               
-
         
-        data=(i[0],i[3],i[4],i[5],i[6],i[7],i[11],i[19],i[20],i[22],i[23],i[32])
-        print(data)
         try:
-            cursor.execute(sql,data)
-            cnx.commit()
-        except mysql.connector.Error as e:
-            print("insert data --",e.msg)
-            breaka
+            print(i[0],'--',i[32])
+            data=(i[0],i[3],i[4],i[5],i[6],i[7],i[11],i[19],i[20],i[22],i[23],i[32])
+            insert()
+            
+        except Exception as e:
+            print('-------------------------------------------',e)
+			
+			
+
         
-        
+
     
-    
-    
-
-
-
-
-
-
-
-'''
-import csv
-with open('d:\Hanson\id.csv', newline='',encoding='utf-8') as f:
-    reader = csv.reader(f)
-    for i in reader:
-        print(i)
-        
-'''
-
-
-'''
-for i in reader:
-        print(i)
-        break
-    
-'''
